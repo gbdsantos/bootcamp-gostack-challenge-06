ad8df9ae0aafd8ea4d5d27f09bffd51dc3ba6182
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-04-23 21:40:38 -0300

    :tada: finish challenge 06

---
+++ README.md
@@ -0,0 +1,48 @@
+<p align="center">
+    <img alt="GoStack" src="https://i.lensdump.com/i/jCEM0c.png" />
+</p>
+
+# :trophy: Challenge 06: Database Update
+
+## :rocket: About Challenge
+
+Using <span style="color:lightgreen; font-weight:bold;">NodeJS</span> and <span style="color:darkslateblue; font-weight:bold;">Typescript</span> to make a transaction management application that stores incoming and outgoing financial transactions and allows the registration and listing of these transactions, in addition to allowing the creation of new records in the database by sending a csv file.
+
+[:brazil: Desafio 06](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-database-upload)
+
+Challenge completed on 2020-04-23.
+
+## Technology
+
+- [NodeJS](https://nodejs.org/en/)
+- [Typescript](https://www.typescriptlang.org/)
+- [TypeORM](https://typeorm.io/)
+- [Docker](https://www.docker.com/)
+- [Postgres](https://www.postgresql.org/)
+
+
+## :gear: Getting Started
+
+```Bash
+# Install dependencies
+yarn install
+
+# Run migrations
+yarn typeorm migration:run
+
+# Run application
+yarn dev:server
+```
+> Access the URL in your browser http://localhost:3333
+
+## Demo
+
+<img alt="Bootcamp GoStack Challenge 06" src="https://i1.lensdump.com/i/jQoH7Z.gif" />
+
+## :man_astronaut: Author
+
+**Guilherme Bezerra** ©️
+
+[![github](http://ap.imagensbrasil.org/images/2018/12/10/github-logo-1.png) ](http://www.github.com/gbdsantos)
+[![gitlab](http://ap.imagensbrasil.org/images/2018/12/10/gitlab-32.png)](https://gitlab.com/gbdsantos1)
+[![linkedin](http://ap.imagensbrasil.org/images/2018/12/10/linkedin-1.png)](https://www.linkedin.com/in/gbdsantos/)
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,83 @@
 import { Router } from 'express';
+import multer from 'multer';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  /** TODO: GET /transactions: Essa rota deve retornar uma listagem com todas as
+   * transações que você cadastrou até agora, junto com o valor da soma de
+   * entradas, retiradas e total de crédito. */
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  /** TODO: POST /transactions:
+   * A rota deve receber title, value, type, e category dentro do corpo da
+   * requisição, sendo o type o tipo da transação, que deve ser income para
+   * entradas (depósitos) e outcome para saídas (retiradas). Ao cadastrar uma
+   * nova transação, ela deve ser armazenada dentro do seu banco de dados,
+   * possuindo os campos id, title, value, type, category_id, created_at, updated_at */
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  /** TODO: DELETE /transactions/:id:
+   * A rota deve deletar uma transação com o id presente nos parâmetros da rota
+   * */
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    /** TODO: POST /transactions/import:
+     * A rota deve permitir a importação de um arquivo com formato .csv contendo
+     * as mesmas informações necessárias para criação de uma transação id, title,
+     * value, type, category_id, created_at, updated_at, onde cada linha do arquivo
+     * CSV deve ser um novo registro para o banco de dados, e por fim retorne todas
+     * as transactions que foram importadas para seu banco de dados.
+     */
+    const importTransactions = new ImportTransactionsService();
+
+    const transactions = await importTransactions.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
